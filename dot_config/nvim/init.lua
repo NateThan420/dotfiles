d17dba4fbdd304787d8eecc5c8a442139bd42875
Nathaniel M. Galimba Jr. <jerkfacegalimba2@gmail.com>
2025-05-19 19:49:16 +0800

    bruh

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,14 +1,3 @@
-require("config.lazy")
 require("nate")
 
---Enable line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
 
---Set tab width and indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.cmd[[colorscheme tokyonight-night]]
-vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
