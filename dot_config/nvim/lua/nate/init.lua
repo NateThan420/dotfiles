ac636c91b2c4a35eafbe30765874d51f92d4d887
NateThan420 <jerkfacegalimba2@gmail.com>
2025-05-21 18:54:40 +0800

    Update init.lua

--- dot_config/nvim/lua/nate/init.lua
+++ dot_config/nvim/lua/nate/init.lua
@@ -1,6 +1,15 @@
 require("config.lazy")
 require("nate.remap")
 
+--Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+--Set tab width and indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.cmd[[colorscheme tokyonight-night]]
 vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
 
