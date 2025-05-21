414efb8c89d7649c28e678e868ff4793995be2c7
NateThan420 <jerkfacegalimba2@gmail.com>
2025-05-21 18:54:54 +0800

    Update remap.lua

--- dot_config/nvim/lua/nate/remap.lua
+++ dot_config/nvim/lua/nate/remap.lua
@@ -1,11 +1,2 @@
---Enable line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
---Set tab width and indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
 vim.g.mapleader = " "
 vim.keymap.set ("n", "<leader>ft", vim.cmd.Ex)
